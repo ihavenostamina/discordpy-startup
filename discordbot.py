ccc7a28fbd16ff8e27fff37ce9da06bdc8796d84
ihavenostamina <66924229+ihavenostamina@users.noreply.github.com>
2020-06-15 08:58:46 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -16,40 +16,6 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
-    
-@bot.command(aliases=["s","summon"]) #connectやsummonでも呼び出せる
-async def join(ctx):
-    """Botをボイスチャンネルに入室させます。"""
-    voice_state = ctx.author.voice
-
-    if (not voice_state) or (not voice_state.channel):
-        await ctx.send("先にボイスチャンネルに入っている必要があります。")
-        return
-
-    channel = voice_state.channel
-
-    await channel.connect()
-    print("connected to:",channel.name)
-
-
-@bot.command(aliases=["disconnect","d"])
-async def leave(ctx):
-    """Botをボイスチャンネルから切断します。"""
-    voice_client = ctx.message.guild.voice_client
-
-    if not voice_client:
-        await ctx.send("Botはこのサーバーのボイスチャンネルに参加していません。")
-        return
-
-    await voice_client.disconnect()
-    await ctx.send("ボイスチャンネルから切断しました。")
-
-
-@bot.command()
-    ffmpeg_audio_source = discord.FFmpegPCMAudio("chanchan.mp3")
-    voice_client.play(ffmpeg_audio_source)
-
-    await ctx.send("再生しました。")
 
 
 bot.run(token)
